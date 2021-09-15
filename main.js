bffc668297111cd94d151a26a957e300398962d8
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-09-15 11:49:16 +0100

    feat: add #print to display field

--- main.js
+++ main.js
@@ -10,4 +10,17 @@ class Field {
     constructor(field){
       this.field = field;
     }
+
+    print(fieldArray=this.field){
+       for(let array of fieldArray){
+         console.log(`${array.join('')}`);
+       }
+    }
 }
+
+//Object instance to test:
+const myField = new Field([['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+myField.print();
