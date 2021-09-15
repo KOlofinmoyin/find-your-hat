12de192f8d84cf9134c212dd042797eec3758343
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-09-15 13:04:59 +0100

    feat: add while condition so when a user runs main.js, they are prompted for input and are able to indicate which direction they’d like to move i.e. l(right), r(right) or d(down) ”

---
+++ README.md
@@ -0,0 +1,6 @@
+# Find Your Hat
+
+An interactive terminal game.
+
+## Scenario:
+The player has lost their hat in a field full of holes, and they must navigate back to it without falling down one of the holes or stepping outside of the field.
--- main.js
+++ main.js
@@ -4,23 +4,28 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+let counter = 0;
 
+while(counter < 3){
+  let direction = prompt("Which way?");
+  
+  class Field {
+      constructor(field){
+        this.field = field;
+      }
 
-class Field {
-    constructor(field){
-      this.field = field;
-    }
+      print(fieldArray=this.field){
+         for(let array of fieldArray){
+           console.log(`${array.join('')}`);
+         }
+      }
+  }
 
-    print(fieldArray=this.field){
-       for(let array of fieldArray){
-         console.log(`${array.join('')}`);
-       }
-    }
+  //Object instance to test:
+  const myField = new Field([['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+  myField.print();
+  counter++;
 }
-
-//Object instance to test:
-const myField = new Field([['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
-]);
-myField.print();
